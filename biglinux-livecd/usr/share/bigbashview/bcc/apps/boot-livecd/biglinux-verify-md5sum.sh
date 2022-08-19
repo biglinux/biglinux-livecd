57b458346925587a23147a908944ac5ca8e15c79
Ruscher <rruscher@gmail.com>
2022-08-19 01:37:48 -0300

    Include checksum and fix CSS

--- biglinux-livecd/usr/share/bigbashview/bcc/apps/boot-livecd/biglinux-verify-md5sum.sh
+++ biglinux-livecd/usr/share/bigbashview/bcc/apps/boot-livecd/biglinux-verify-md5sum.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 ##################################
 #  Author1: Bruno Goncalves (www.biglinux.com.br) 
 #  Author2: Rafael Ruscher (rruscher@gmail.com)  
-#  Date:    2022/08/13 
+#  Date:    2022/08/19
 #  
 #  Description: Control Center to help usage of BigLinux 
 #  
@@ -17,10 +16,12 @@ export LANG=$(cat /tmp/big_language).UTF-8
 export TEXTDOMAINDIR="/usr/share/locale"
 export TEXTDOMAIN=biglinux-livecd
 
+
+
 File="/tmp/checksum_biglinux.html"
 FileVerified="/tmp/checksum_biglinux_ok.html"
-Error=$"Erro encontrado, baixe novamente o sistema ou utilize outro pendrive."
-Message=$"Verificando a integridade do sistema:"
+Error=$"Erro."
+MessageFinish=$"O sistema passou no teste de integridade."
 
 if [ ! -e $FileVerified ]; then
 
@@ -28,38 +29,49 @@ if [ ! -e $FileVerified ]; then
     cd /run/miso/bootmnt/manjaro/x86_64/
     #cd /home/biglinux/x86_64/
 
-    #Message=$"Verificando o arquivo:"
-    #Message2=$"Verificando se ocorreram erros no download ou pendrive, pode demorar alguns minutos..."
-
-    echo "$Message 10%" > $File
+    Message=$"Verificando a integridade do sistema:"
+    
+    echo '<script>$( function() {$( "#progressbar").progressbar({value: 15});} );$( function() {$("#textprogress").text("'$Message' 15%");} );$(".ui-widget-header").css("background", "#2196f3");</script>' > $File
     md5sum --status -c desktopfs.md5
+
     if [ "$?" != "0" ] ; then
-        echo "$Error" > $File
+        echo '<script>$( function() {$( "#progressbar").progressbar({value: 100});} );$( function() {$("#textprogress").text("'$Error'");} );$(".ui-widget-header").css("background", "red"); </script>' > $File
+        ./erro.sh
         exit
     fi
 
-    echo "$Message 50%" > $File
+    echo '<script>$( function() {$( "#progressbar").progressbar({value: 35});} );$( function() {$("#textprogress").text("'$Message' 35%");} );$(".ui-widget-header").css("background", "#2196f3");</script>' > $File
     md5sum --status -c livefs.md5
     if [ "$?" != "0" ] ; then
-        echo "$Error" > $File
+        echo '<script>$( function() {$( "#progressbar").progressbar({value: 100});} );$( function() {$("#textprogress").text("'$Error'");} );$(".ui-widget-header").css("background", "red"); </script>' > $File
+        ./erro.sh
         exit
     fi
 
-    echo "$Message 60%" > $File
+    echo '<script>$( function() {$( "#progressbar").progressbar({value: 50});} );$( function() {$("#textprogress").text("'$Message' 50%");} );$(".ui-widget-header").css("background", "#2196f3");</script>' > $File
     md5sum --status -c mhwdfs.md5
     if [ "$?" != "0" ] ; then
-        echo "$Error" > $File
+        echo '<script>$( function() {$( "#progressbar").progressbar({value: 100});} );$( function() {$("#textprogress").text("'$Error'");} );$(".ui-widget-header").css("background", "red"); </script>' > $File
+        ./erro.sh
         exit
     fi
 
-    echo "$Message 80%" > $File
+    echo '<script>$( function() {$( "#progressbar").progressbar({value: 75});} );$( function() {$("#textprogress").text("'$Message' 75%");} );$(".ui-widget-header").css("background", "#2196f3");</script>' > $File
     md5sum --status -c rootfs.md5
     if [ "$?" != "0" ] ; then
-        echo "$Error" > $File
+        echo '<script>$( function() {$( "#progressbar").progressbar({value: 100});} );$( function() {$("#textprogress").text("'$Error'");} );$(".ui-widget-header").css("background", "red"); </script>' > $File
+        ./erro.sh
         exit
     fi
-
+    
+    echo '<script>$( function() {$( "#progressbar").progressbar({value: 90});} );$( function() {$("#textprogress").text("'$Message' 90%");} );$(".ui-widget-header").css("background", "#2196f3");</script>' > $File
+    sleep 2  
 fi
 
-    echo $"O sistema passou no teste de integridade." > $File
-    echo 1 > $FileVerified
+echo '<script>
+$( function() {$( "#progressbar").progressbar({value: 100});} );
+$( function() {$("#textprogress").text("'$MessageFinish'");} );
+$(".ui-widget-header").css("background", "#2196f3");
+</script>' > $File    
+
+echo 1 > $FileVerified
