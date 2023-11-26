103f4f220dee939779620f282b86e87a1f3b816b
talesam <talesam@gmail.com>
2023-11-25 22:49:54 -0300

    Reworked entire startbiglive and added option for a wayland session to choose language and keyboard.

--- biglinux-livecd/usr/share/bigbashview/bcc/apps/boot-livecd/run.sh
+++ biglinux-livecd/usr/share/bigbashview/bcc/apps/boot-livecd/run.sh
@@ -1,3 +1,12 @@
-/usr/bin/bigbashview -s $(LANG=C xdpyinfo  | grep 'dimensions:' | sed 's|.*dimensions:||g;s| pixels.*||g') /usr/share/bigbashview/bcc/apps/boot-livecd/index.sh.htm
-chmod +x /tmp/biglightdm
-/tmp/biglightdm
+#!/usr/bin/env bash
+
+# Check the session type and move to the appropriate directory
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+  /usr/bin/bigbashview -s $(LANG=C xdpyinfo  | grep 'dimensions:' | sed 's|.*dimensions:||g;s| pixels.*||g') /usr/share/bigbashview/bcc/apps/boot-livecd/index.sh.htm
+  chmod +x /tmp/biglightdm
+  /tmp/biglightdm
+elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    /usr/bin/bigbashview -w fullscreen /usr/share/bigbashview/bcc/apps/boot-livecd/index.sh.htm
+    chmod +x /tmp/biglightdm
+    /tmp/biglightdm
+fi
